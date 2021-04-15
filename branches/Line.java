df86f6dc97e54d6451d6b0963b88e1ac345ac8d5
il01193 <il01193@1c858e48-00fc-4f44-9443-613b170f67d3>
2021-04-15 12:24:57 +0000

    Update: Suivi de ligne fonctionnel. Adapter avec les autres class

--- branches/Line.java
+++ branches/Line.java
@@ -1,31 +1,52 @@
 package codesource;
 
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.robotics.SampleProvider;
+
+
 import lejos.utility.Delay;
 
 public class Line {
-	//Attributs
-	private String couleur;
-	private int ligne;
-	
-	//Constructeurs
-	public Line(String couleur, int ligne) {
-		this.couleur = couleur;
-		this.ligne = ligne;
-		ligne = 1;
-	}
-	
-	public void Follower(couleur) {
-		if (couleur=="JAUNE") {
-			Motor.Straight();
-		}
-		else {
-			Motor.Left();
-			Motor.Right();
-		}
-		
-		ligne += 1;
-	}
-
+    
+    Motor motor=new Motor();
+    ColorSensor col=new ColorSensor();
+    String couleur=col.getColor();
+    
+    public void line() {
+        while(true) { //Ici on fait tourner la boucle à l'infini pour tester
+        	while(!couleur.equals("GRIS")) { //Tant que le robot est sur une ligne
+	        	motor.setMotorGSpeed(400);//Le robot dérive un peu plus à droite
+	        	motor.setMotorDSpeed(352);
+	        	
+	            motor.Straight();
+	            couleur=col.getColor();
+            
+            while(couleur.equals("GRIS")) { //Tant que le robot voit du GRIS (hors de la ligne)
+	            motor.setMotorGSpeed(350);//Le robot dérive un peu plus à gauche
+	            motor.setMotorDSpeed(400);
+	            motor.Straight();
+	            //motor.Left(0.5);
+	            couleur=col.getColor();
+	            
+                
+            	}
+        	}
+            
+            couleur=col.getColor();
+            
+        }
+    }
+    
+    public static void main(String[]args){
+    	
+    	//Line test1=new Line();
+    	
+    	//test1.testColor();
+    	System.out.println("1");
+        Delay.msDelay(500);
+        Line test=new Line();
+        System.out.println("2");
+        Delay.msDelay(500);
+        test.line();
+        System.out.println("5");
+        Delay.msDelay(500);
+    }
+}    
