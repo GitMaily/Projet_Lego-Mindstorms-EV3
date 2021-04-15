685e7cc79a7f39bddea1d5243758b8a2eaabb615
il01193 <il01193@1c858e48-00fc-4f44-9443-613b170f67d3>
2021-04-15 14:31:31 +0000

    Update 2: Réglage de la boucle infinie (à tester)

--- branches/Line.java
+++ branches/Line.java
@@ -10,17 +10,17 @@ public class Line {
     ColorSensor col=new ColorSensor();
     String couleur=col.getColor();
     
-    public void line() {
-        while(true) { //Ici on fait tourner la boucle Ã  l'infini pour tester
-        	while(!couleur.equals("GRIS")) { //Tant que le robot est sur une ligne
-	        	motor.setMotorGSpeed(400);//Le robot dÃ©rive un peu plus Ã  droite
+    public void line(String coul) { //assigner coul
+        while(couleur.equals(coul) || couleur.equals("GRIS")) { //tant que coul ou gris
+        	while(couleur.equals(coul)) { //Tant que le robot est sur la ligne coul
+	        	motor.setMotorGSpeed(400);//Le robot dérive un peu plus à droite
 	        	motor.setMotorDSpeed(352);
 	        	
 	            motor.Straight();
 	            couleur=col.getColor();
             
             while(couleur.equals("GRIS")) { //Tant que le robot voit du GRIS (hors de la ligne)
-	            motor.setMotorGSpeed(350);//Le robot dÃ©rive un peu plus Ã  gauche
+	            motor.setMotorGSpeed(350);//Le robot dérive un peu plus à gauche
 	            motor.setMotorDSpeed(400);
 	            motor.Straight();
 	            //motor.Left(0.5);
@@ -45,7 +45,7 @@ public class Line {
         Line test=new Line();
         System.out.println("2");
         Delay.msDelay(500);
-        test.line();
+        test.line("JAUNE");
         System.out.println("5");
         Delay.msDelay(500);
     }
